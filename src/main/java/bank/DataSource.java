b99d6b7cccfe42c7744449d4b669b529bb78ce02
comodoropet4 <16455591+comodoropet4@users.noreply.github.com>
2024-08-07 06:08:26 +0000

    Set up a method to get a customer by its username

--- src/main/java/bank/DataSource.java
+++ src/main/java/bank/DataSource.java
@@ -2,10 +2,12 @@ package bank;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 
 public class DataSource {
-  
+
   public static Connection connect() {
     String db_file = "jdbc:sqlite:resources/bank.db";
     Connection connection = null;
@@ -15,11 +17,38 @@ public class DataSource {
     } catch(SQLException e){
       e.printStackTrace();
     }
-    
+
     return connection;
   }
-  
+
+  public static Customer getCustomer(String customerUsername) {
+    String sqlCustomerQuery = "SELECT * FROM customers WHERE username = ?";
+    Customer retrievedCustomer = null;
+
+    try (
+      Connection bankDatabaseConnection = connect();
+      PreparedStatement customerQueryStatement = bankDatabaseConnection.prepareStatement(sqlCustomerQuery)
+    ) {
+      customerQueryStatement.setString(1, customerUsername);
+
+      try(ResultSet customerResultSet = customerQueryStatement.executeQuery()) {
+        retrievedCustomer = new Customer(
+          customerResultSet.getInt("id"),
+          customerResultSet.getString("name"),
+          customerResultSet.getString("username"),
+          customerResultSet.getString("password"),
+          customerResultSet.getInt("account_id")
+        );
+      }
+    } catch (SQLException exception) {
+      exception.printStackTrace();
+    }
+
+    return retrievedCustomer;
+  }
+
   public static void main(String[] args){
-    connect();
+    Customer exampleCustomer = getCustomer("twest8o@friendfeed.com");
+    System.out.println(exampleCustomer.getName());
   }
 }
